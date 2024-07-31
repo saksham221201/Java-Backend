f1d85381f5f2a8041426b4cbb352b30922e3cd5d
sakshamjoshi <sakshamjoshi1975@gmail.com>
2024-07-31 18:20:40 +0530

    Employee Module Project Initiated

---
+++ employee-module/src/main/java/com/nagarro/employee_module/EmployeeModuleApplication.java
@@ -0,0 +1,13 @@
+package com.nagarro.employee_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeModuleApplication.class, args);
+	}
+
+}
