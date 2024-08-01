b36359e409ff13d5ee73c2f894fef3dd182f7487
kamadnayyar <kamad.nayyar@nagarro.com>
2024-08-01 11:51:58 +0530

    service impl

--- employee-module/src/main/java/com/nagarro/employee_module/service/EmployeeService.java
+++ employee-module/src/main/java/com/nagarro/employee_module/service/EmployeeService.java
@@ -1,4 +1,7 @@
 package com.nagarro.employee_module.service;
 
+import com.nagarro.employee_module.entity.Employee;
+
 public interface EmployeeService {
+    public Employee createEmployee(Employee employee);
 }
--- employee-module/src/main/java/com/nagarro/employee_module/service/impl/EmployeeServiceImpl.java
+++ employee-module/src/main/java/com/nagarro/employee_module/service/impl/EmployeeServiceImpl.java
@@ -1,8 +1,15 @@
 package com.nagarro.employee_module.service.impl;
 
+import com.nagarro.employee_module.entity.Employee;
 import com.nagarro.employee_module.service.EmployeeService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
+
+
+    @Override
+    public Employee createEmployee(Employee employee) {
+            
+    }
 }
