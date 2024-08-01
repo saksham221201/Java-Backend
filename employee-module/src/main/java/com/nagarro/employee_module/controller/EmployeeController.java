c5e228a08be4fa0774dd4647436b01afc06ebeb5
sakshamjoshi <sakshamjoshi1975@gmail.com>
2024-08-01 16:06:54 +0530

    Resolved DTO Conflicts

--- employee-module/src/main/java/com/nagarro/employee_module/controller/EmployeeController.java
+++ employee-module/src/main/java/com/nagarro/employee_module/controller/EmployeeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/v1/api/employees")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
@@ -18,17 +19,17 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<EmployeeDTO>> getAllEmployees() {
+        List<EmployeeDTO> employees = employeeService.getAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
 
-//    public ResponseEntity<List<EmployeeDTO>> getAllEmployees() {
-//        List<EmployeeDTO> employees = employeeService.getAllEmployees();
-//        return new ResponseEntity<>(employees, HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/id/{}")
-//    public ResponseEntity<EmployeeDTO> getEmployeeById(@PathVariable int employeeId) {
-//        EmployeeDTO employee = employeeService.getEmployeeById(employeeId);
-//        return new ResponseEntity<>(employee, HttpStatus.OK);
-//    }
+    @GetMapping("/id/{}")
+    public ResponseEntity<EmployeeDTO> getEmployeeById(@PathVariable int employeeId) {
+        EmployeeDTO employee = employeeService.getEmployeeById(employeeId);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
 
     @PostMapping
     public ResponseEntity<Employee> createNewEmployee(@RequestBody EmployeeDTO employeeDTO){
